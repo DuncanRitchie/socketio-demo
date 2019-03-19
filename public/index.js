aa8255de1c2905d12ab8441eedbec930a4395658
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-19 16:48:48 +0000

    Now works in multiple windows, and has a coral explanation.

--- public/index.js
+++ public/index.js
@@ -1,15 +1,16 @@
 const socket = io()
 
-socket.on("message",(text)=>{
-    console.log(text)
-})
-
 input = document.getElementById("input");
 button = document.getElementById("button")
+paragraph = document.getElementById("paragraph")
+
+socket.on("message",(text)=>{
+    console.log(text)
+    paragraph.innerHTML+=text+"<br />"
+})
 
 button.addEventListener("click",(e)=>{
     e.preventDefault();
-    console.log(input.value)
     socket.emit("message",input.value)
     input.value=""
 })
