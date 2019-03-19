37b1a74238543f099d1af95b33082cf90e88c5f1
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-19 16:10:46 +0000

    Input now logs to console on submit.

--- public/index.js
+++ public/index.js
@@ -3,3 +3,12 @@ const socket = io()
 socket.on("message",(text)=>{
     console.log(text)
 })
+
+input = document.getElementById("input");
+button = document.getElementById("button")
+
+button.addEventListener("click",(e)=>{
+    e.preventDefault();
+    console.log(input.value)
+    input.value=""
+})
