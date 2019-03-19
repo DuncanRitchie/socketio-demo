2d889c50af0fce04e3d06dec1662f974c0e84eb4
DuncanRitchie <duncanritchie@btinternet.com>
2019-03-19 15:39:36 +0000

    Starting with Git/Node/Express/Socket.io.

---
+++ src/chat.js
@@ -0,0 +1,19 @@
+const path = require("path")
+const publicDir = path.join(__dirname,"../public");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const http = require("http");
+const server = http.createServer(app)
+const socketio = require("socket.io")
+const io = socketio(server)
+
+app.use(express.static(publicDir))
+
+io.on("connection",()=>{
+    console.log("New connection with WebSocket")
+})
+
+server.listen(port,()=>{
+    console.log(`Listening on port ${port}`)
+})
